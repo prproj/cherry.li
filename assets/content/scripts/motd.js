f23ba5bdef04201911d19986319da6e30150a7c4
cherrypluck <101410491+cherrypluck@users.noreply.github.com>
2026-01-09 20:48:08 +0530

    *

--- assets/content/scripts/motd.js
+++ assets/content/scripts/motd.js
@@ -1,17 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('../assets/content/misc/motd.txt')  // Replace with the path to your MOTD text file
+    fetch('../assets/content/misc/motd.txt')
         .then(response => response.text())
         .then(data => {
-            // Split text into lines (assuming each line is a MOTD)
             const motds = data.trim().split('\n');
-            // Get a random MOTD from the array
             const randomIndex = Math.floor(Math.random() * motds.length);
             const randomMotd = motds[randomIndex];
-            // Update the paragraph element with the random MOTD
             document.getElementById('motd').textContent = randomMotd;
         })
         .catch(error => {
             console.error('Error fetching and parsing MOTD:', error);
             document.getElementById('motdParagraph').textContent = 'Failed to fetch MOTD';
         });
+
 });
